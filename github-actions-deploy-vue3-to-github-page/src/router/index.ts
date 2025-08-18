e640c09df0c5c55ed3746c7e2b269bdd508115a4
congweiH <congwei_huang@163.com>
2025-08-19 02:29:00 +0800

    调试

--- github-actions-deploy-vue3-to-github-page/src/router/index.ts
+++ github-actions-deploy-vue3-to-github-page/src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory('/project-demos/github-actions-deploy-vue3-to-github-page/'),
   routes: [],
 })
 
